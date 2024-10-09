0449ea592e6a427c91bae68b0d51ce97b12ef8d3
Brian Cheng <brian_cheng@ucsb.edu>
2024-10-09 16:38:56 -0700

    xy - updated for full line and mutation coverage for Team.java

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -19,8 +19,31 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
 
+    @Test
+    public void equals_returns_correct_boolean() {
+        assertEquals(true, team.equals(team));
+        assertEquals(false, team.equals(null));
+
+        Team team2 = new Team("test-team");
+        assertEquals(true, team.equals(team2));
+
+        team2.addMember("test-member");
+        assertEquals(false, team.equals(team2));
+
+        team2.setName("diff-team");
+        assertEquals(false, team.equals(team2));
+    }
+
+    @Test
+    public void hashCode_returns_correct_hashcode() {
+        Team t = new Team();
+        int result = t.hashCode();
+        int expectedResult = 1;
+        assertEquals(expectedResult, result);
+    }
 }
